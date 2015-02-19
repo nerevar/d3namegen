aa023ff3ab8bf62035c61d50103e5373888093b7
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2015-02-19 12:19:34 -0500

    update files

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 (function($){
 
     $(function(){
-
-        onStateChange();
-
-        History.Adapter.bind(window, 'statechange', onStateChange);
-
         $('.d3button').on('click', generate);
         
-    });
-
-    function onStateChange() {
+        _onStateChange();
         
-        var wordId = window.location.href.split('?')[1];
+        History.Adapter.bind(window, 'statechange', _onStateChange);
+    });
+    
+    function _onStateChange() {
+        
+        var state = History.getState(),
+            wordId = window.location.href.split('?')[1];
+            
+        window.yaCounter18037942 && window.yaCounter18037942.hit(window.location.href);
             
         if (wordId) {
             (isNaN(wordId) || wordId.length !== 10)
@@ -48,7 +49,7 @@
             wordIds = [
                 zeroPad(Math.floor(Math.random() * words.prefix[gender].length), 3),
                 zeroPad(Math.floor(Math.random() * words.base[gender].length), 3),
-                zeroPad(Math.floor(Math.random() * words.postfix.length), 3)
+                zeroPad(Math.floor(Math.random() * words.postfix.length), 3),
             ],
             wordId = [genderId+1].concat(wordIds).join(''),
             item;
@@ -70,87 +71,49 @@
     }
     
     function display(item) {
-
         item = item.charAt(0).toUpperCase() + item.slice(1);
     
         $('.b-item').text(item);
-
-        updateTwitter(item);
-
-        updateVkontakte(item);
-
-    }
-
-    function change(wordId) {
-        History.pushState({}, '', '?' + wordId);
-    }
-
-    function updateTwitter(text) {
-
-        $('.b-share__button_type_twitter').html(
-            $('<a></a>')
-                .text('Твитнуть')
-                .attr({
-                    href: 'https://twitter.com/share',
-                    'class': 'twitter-share-button',
-                    'data-text': text + ' #diablo3',
-                    'data-lang': 'ru',
-                    'data-hashtags': 'd3name.ru',
-                    'data-count': 'none'
-                })
-        );
-
+        
+        $('.b-share__button_type_twitter').html('<a href="https://twitter.com/share" class="twitter-share-button" data-text="' + item + ' #diablo3' + '" data-lang="ru" data-hashtags="d3name.ru" data-count="none">Твитнуть</a>');
         window.twttr && window.twttr.widgets.load();
-
-    }
-
-    function updateVkontakte(text) {
-
+        
         $('.b-share__button_type_vk').html(VK.Share.button({
             url: window.location.href,
-            title: text,
+            title: item,
             description: ' — Генератор названий предметов Diablo 3 — http://d3name.ru',
             image: 'http://d3name.ru/img/d3small.jpg',
             noparse: true
         }, {
-            type: "round_nocount",
+            type: "round_nocount", 
             text: "Поделиться"
         }));
-
     }
-
+    
     function zeroPad(number, length) {       
-
         var str = '' + number;
-
         while (str.length < length) {
             str = '0' + str;
         }
-
         return str;
-
     }
-
-    // cache arrays length into closure variables
-    var maleWordsLength = words.base.male.length,
-        femaleWordsLength = words.base.female.length,
-        neuterWordsLength = words.base.neuter.length,
-        manyWordsLength = words.base.many.length,
-        totalBaseWordsCount = maleWordsLength + femaleWordsLength + neuterWordsLength + manyWordsLength;
-
+    
+    function change(wordId) {
+        History.pushState({}, '', '?' + wordId);
+    }
+    
     function getRandomGender() {
-
-        var randomBaseWordId = Math.floor(Math.random() * totalBaseWordsCount),
-            gender = randomBaseWordId <= maleWordsLength
-                ? 'male'
-                : randomBaseWordId <= maleWordsLength + femaleWordsLength
-                    ? 'female'
-                    : randomBaseWordId <= maleWordsLength + femaleWordsLength + neuterWordsLength
-                        ? 'neuter'
-                        : 'many';
+        var totalBaseWordsCount = words.base.male.length + words.base.female.length + words.base.neuter.length + words.base.many.length,
+        randomBaseWordId = Math.floor(Math.random() * totalBaseWordsCount),
+        gender = randomBaseWordId <= words.base.male.length
+            ? 'male'
+            : randomBaseWordId <= words.base.male.length + words.base.female.length
+                ? 'female'
+                : randomBaseWordId <= words.base.male.length + words.base.female.length + words.base.neuter.length
+                    ? 'neuter'
+                    : 'many';
                     
         return gender;
-
     }
     
 })(jQuery);
